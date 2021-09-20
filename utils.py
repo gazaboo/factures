69e1a3ac9d73ff929ff602821320dd74ee87d2ab
Florian Dadouchi <florian.dadouchi@gmail.com>
2021-09-20 14:48:13 +0200

    fix int nbre jour prepa add promo

--- utils.py
+++ utils.py
@@ -10,9 +10,10 @@ def get_info(req):
         "prix_journalier":450, 
         "date_emission_facture":dt.datetime.today().strftime("%d-%m-%Y"),
         "qte": len(req.get("dates").split(",")),
-        "nbre_jour_prepa": int(req.get("nbre_jours_prepa")), 
+        "nbre_jour_prepa": float(req.get("nbre_jours_prepa")), 
         "webbrowser": True, 
-        "num_facture": dt.datetime.today().strftime("%M%S%f")
+        "num_facture": dt.datetime.today().strftime("%M%S%f"), 
+        "nom_promotion": req.get("nom_promotion")
     }
     data["prix_preparation"] = data['nbre_jour_prepa']*data['prix_journalier']
     data["prix_total_animation"] = len(data["date_cours"])*data['prix_journalier']
